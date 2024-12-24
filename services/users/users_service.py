3ad11ec8e75ee9205f3f819be28c8d85d6e5d9f4
in7hesky <anton.maiko.dev@gmail.com>
2024-12-24 14:56:04 +0300

    add pydantic validation

--- services/users/users_service.py
+++ services/users/users_service.py
@@ -1,7 +1,9 @@
 import requests
+from requests import Response
 from services.users.data.payloads import Payloads
 from services.users.data.endpoints import Endpoints
 from config.headers import Headers
+from models.users.user_model import UserModel
 
 
 class UsersService:
@@ -11,10 +13,13 @@ class UsersService:
         self.endpoints = Endpoints()
         self.headers = Headers()
 
-    def create_user(self):
-        print(self.endpoints.create_user)
-        return requests.post(
+    def create_user(self) -> Response:
+        response =  requests.post(
             url=self.endpoints.create_user,
             headers=self.headers.basic_auth,
             json=self.payloads.create_user
         )
+
+        UserModel.model_validate(response.json())
+
+        return response
--- tests/test_users.py
+++ tests/test_users.py
@@ -5,6 +5,4 @@ class TestUsersService(BaseTest):
 
     def test_create_user(self):
         response = self.users_service.create_user()
-        print(response.json())
         assert response.status_code == 200, response.json()
-
